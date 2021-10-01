898241337b6acf6e8d3fca56b28d9bae2890204e
Sofi1410 <sofy.bobadilla@gmail.com>
2021-10-01 14:46:02 -0300

    Se agrega DD y passan los test de enunciado

---
+++ src/Meter.java
@@ -0,0 +1,51 @@
+import java.util.Objects;
+
+public class Meter implements IUnit {
+    private double value ;
+    public Meter(double medida) {
+        value=medida;
+    }
+
+    //metroA.add(metroB) -> 2 m + 4 m
+
+    // metroA.add(inchb) -> 2m + 1 inch
+    public Meter add(IUnit unidad){
+        return unidad.addToAMeter(this);
+
+    }
+
+    public Meter add(Inch pulgada) {
+        Meter resultado= new Meter(
+                value+ pulgada.getValue()* 0.0254 ) ;
+        return resultado;
+    }
+
+    //nos estamos sumando con un metro
+    public Meter addToAMeter(Meter meter) {
+        Meter resultado= new Meter(
+                value+ meter.getValue()) ;
+        return resultado;
+    }
+
+
+    // Meter + Inch
+
+    @Override
+    public Inch addToAInch(Inch inch) {
+        Inch result = new Inch(inch.getValue() + this.value*39.37);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Meter)) return false;
+        Meter meter = (Meter) o;
+        return Double.compare(meter.value, value) == 0;
+    }
+
+
+    public double getValue() {
+        return value;
+    }
+}
--- test/MetricsTest.java
+++ test/MetricsTest.java
@@ -1,4 +1,54 @@
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 public class MetricsTest {
+    private Inch prueba1;
+    private Meter prueba2;
+
+    @Before
+    public void setUp(){
+        //prueba1 = new Inch();
+        //prueba2= new Meter();
+    }
+
+    @Test
+    public void addInch(){
+        Meter nuevoMetro= new Meter(1);
+        Inch nuevaPulgada= new Inch(39);
+        Meter resultado = nuevoMetro.add(nuevaPulgada);
+        Meter expected= new Meter(1.9906 );
+        assertTrue(expected.equals(resultado));
+    }
+
+    @Test
+    public void addMeter(){
+        Meter nuevoMetro= new Meter(2);
+        Inch nuevaPulgada= new Inch(10);
+        Inch resultado= nuevaPulgada.add(nuevoMetro);
+        Inch expected= new Inch(88.74);
+        assertTrue(expected.equals(resultado));
+    }
+
+    @Test
+    public void  MeteraddMeter(){
+        Meter nuevoMetro= new Meter(5);
+        Meter nuevoMetro2= new Meter(3);
+        Meter resultado = nuevoMetro.add(nuevoMetro2);
+        Meter expected= new Meter(8);
+        assertTrue(expected.equals(resultado));
+    }
+
+    @Test
+    public void InchaddInch(){
+        Inch nuevaPulgada= new Inch(3);
+        Inch nuevaPulgada2= new Inch(10);
+        Inch resultado= nuevaPulgada.add(nuevaPulgada2);
+        Inch expected= new Inch(13);
+        assertTrue(expected.equals(resultado));
+    }
+
+
 
 }
